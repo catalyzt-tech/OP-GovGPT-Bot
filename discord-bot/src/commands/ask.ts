5a3e7c450d325bbb1dabcf2d03ff880c771c960c
Billy19191 <bill.ratchanon@gmail.com>
2024-09-18 00:40:29 +0700

    feat & fix: add axios, handle fetching error, remove citation

--- discord-bot/src/commands/ask.ts
+++ discord-bot/src/commands/ask.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js'
 import { APIResponse } from '../types/types'
+import axios from 'axios'
 import 'dotenv/config'
 
 export const data = new SlashCommandBuilder()
@@ -12,36 +13,39 @@ export const data = new SlashCommandBuilder()
       .setRequired(true)
   )
 
-export async function execute(interaction: any) {
-  try {
-    const question: string = interaction.options.getString('question') as string
-    console.log(question)
-    await interaction.deferReply()
-    const answer: APIResponse = await fetch(
+export async function execute(interaction: any): Promise<void> {
+  const question: string = interaction.options.getString('question')
+  console.log(`Question: ${question}`)
+  await interaction.deferReply()
+  const response = await axios
+    .post<APIResponse>(
       process.env.API_ENDPOINT as string,
+      { question },
       {
-        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ question }),
       }
-    ).then((res) => res.json())
-    setTimeout(() => {}, 2000)
-    // if (answer.result.output.length > 2000) {
-    //   return await interaction.editReply(answer.result.output.slice(0, 2000))
-    // }
-    const resultData: string =
-      answer.result.output +
-      '\n\n' +
-      '**Citation:**' +
-      '\n' +
-      answer.links.map((link) => `• ${link}`).join('\n')
-    return await interaction.editReply(resultData)
-  } catch (error) {
-    console.error(error)
-    throw new Error(
-      'There was an error while processing your request, please try again'
     )
-  }
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error(error)
+      return {
+        result: { output: 'Error fetching response, please try again' },
+        links: [],
+      }
+    })
+
+  setTimeout(() => {}, 1000)
+  // if (answer.result.output.length > 2000) {
+  //   return await interaction.editReply(answer.result.output.slice(0, 2000))
+  // }
+  // const resultData: string =
+  //   response.result.output +
+  //   '\n\n' +
+  //   '**Citation:**' +
+  //   '\n' +
+  //   response.links.map((link) => `• ${link}`).join('\n')
+  const resultData: string = response.result.output
+  return await interaction.editReply(resultData)
 }
